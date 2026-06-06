5fdae23c0695d8905129d623cd1429d0acc3499e
Janith Rangika <83299463+Artemis23710@users.noreply.github.com>
2026-06-06 13:23:39 +0530

    06-06 Project Config

--- routes/vehicle.php
+++
@@ -1,8 +0,0 @@
-<?php
-use App\Http\Controllers\VehicleController;
-use App\Http\Controllers\VehicleLoadingController;
-use Illuminate\Support\Facades\Route;
-
-Route::resource('vehicles', VehicleController::class);
-Route::get('vehicles-data', [VehicleController::class, 'getVehiclesData'])->name('vehicles.data');
-Route::post('vehicles/{id}/status', [VehicleController::class, 'updateStatus'])->name('vehicles.status');
