a71a8dfbc9aad3458e911c0cde1bee706cb3647b
Chamith Dilshan Tharuka <dilshanchamith99@gmail.com>
2026-06-22 10:15:27 +0530

    Add models and migrations for EmployeeShift, Shift, and ShiftType

---
+++ app/Models/ShiftManagement/Shift.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\ShiftManagement;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shift extends Model
+{
+    use HasFactory;
+
+    protected $table = 'shifts';
+
+    protected $fillable = [
+        'emp_id',
+        'shift_id',
+        'shift_location',
+        'start_time',
+        'end_time',
+    ];
+}
