2cde8689924a50cf05a149c7534955a782d410e3
Namidu99 <nami.hasalanka@gmail.com>
2026-06-22 22:51:29 +0530

    Add ExamSubject model and migration for exam_subjects table

---
+++ app/Models/EmpMaster/ExamSubject.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\EmpMaster;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExamSubject extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'exam_type',
+        'subject',
+        'status',
+        'created_by',
+        'updated_by',
+    ];
+}
