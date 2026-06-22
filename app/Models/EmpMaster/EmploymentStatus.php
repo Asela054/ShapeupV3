112a79c4641631fd182337cf6ccd3c08890bcf72
Namidu99 <nami.hasalanka@gmail.com>
2026-06-22 22:51:16 +0530

    Add EmploymentStatus model and migration for employment_statuses table

---
+++ app/Models/EmpMaster/EmploymentStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\EmpMaster;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EmploymentStatus extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'emp_status',
+    ];
+}
