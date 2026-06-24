d9d020a7b234c3c60fda12889a43443f3729f2c4
Chamith Dilshan Tharuka <dilshanchamith99@gmail.com>
2026-06-24 21:13:00 +0530

    Add migration for employee_work_rates_backup_records table

---
+++ database/migrations/2026_06_24_144227_create_employee_work_rates_backup_records_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('employee_work_rates_backup_records', function (Blueprint $table) {
+            $table->id();
+            $table->integer('record_id');
+            $table->integer('emp_id');
+            $table->string('emp_etfno')->nullable();
+            $table->integer('work_year');
+            $table->integer('work_month');
+            $table->double('work_days');
+            $table->double('working_week_days')->default(0);
+            $table->double('work_hours');
+            $table->double('leave_days');
+            $table->double('nopay_days');
+            $table->double('emp_late_hours')->default(0);
+            $table->double('normal_rate_otwork_hrs');
+            $table->double('double_rate_otwork_hrs');
+            $table->double('triple_rate_otwork_hrs')->default(0);
+            $table->double('holiday_nopay_days')->default(0);
+            $table->double('holiday_normal_ot_hrs')->default(0);
+            $table->double('holiday_double_ot_hrs')->default(0);
+            $table->string('created_by');
+            $table->string('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('employee_work_rates_backup_records');
+    }
+};
