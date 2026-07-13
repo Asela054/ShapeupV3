3856e497312f5813ed18e2fea4532b3bfe5902a8
Namidu99 <nami.hasalanka@gmail.com>
2026-07-13 13:26:56 +0530

    Refactor Company model to streamline fillable properties and fix typo in field names

--- app/Models/Organization/Company.php
+++ app/Models/Organization/Company.php
@@ -9,31 +9,20 @@ class Company extends Model
 {
     use HasFactory;
 
-     protected $fillable = [
-        'name',
-        'code',
-        'logo',
-        'address',
-        'mobile',
-        'land',
-        'email',
-        'domain_name',
-        'epf',
-        'etf',
-        'employer_number',
-        'zone_code',
-        'ref_no',
-        'vat_reg_no',
-        'svat_no',
-        'company_type',
-        'paysheet_language',
-        'status',
-        'create_by',
-        'update_by',
+    protected $fillable = [
+        'name', 'code', 'logo', 'address', 'mobile', 'land', 'email',
+        'domain_name', 'epf', 'etf', 'employer_number', 'zone_code',
+        'ref_no', 'vat_reg_no', 'svat_no', 'company_type',
+        'paysheet_language', 'status', 'created_by', 'updated_by',
     ];
 
-     protected $casts = [
+    protected $casts = [
         'company_type' => 'integer',
         'paysheet_language' => 'integer',
     ];
+
+    public function bankDetails()
+    {
+        return $this->hasMany(CompanyBankDetail::class, 'company_id');
+    }
 }
