e61c7372f749beec2382cab4920fa6b3c7fec2b2
Namidu99 <nami.hasalanka@gmail.com>
2026-07-31 13:03:30 +0530

    Add employeeBanks relationship to Bank and BankBranch models

--- app/Models/Organization/Bank.php
+++ app/Models/Organization/Bank.php
@@ -4,6 +4,7 @@ namespace App\Models\Organization;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\EmpDetail\EmployeeBank;
 
 class Bank extends Model
 {
@@ -17,4 +18,12 @@ class Bank extends Model
         'created_by',
         'updated_by',
     ];
+
+    /**
+     * banks.code → employee_banks.bank_code
+     */
+    public function employeeBanks()
+    {
+        return $this->hasMany(EmployeeBank::class, 'bank_code', 'code');
+    }
 }
